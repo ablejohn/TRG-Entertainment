61b69d463667ce2f7e4957af52ddff06e1794a33
ABLE JOHN <johnabe410@gmail.com>
2024-12-26 01:10:47 +0100

    landing page and navigation commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styling/navbar.css";
-import logo from "../assets/logo.jpg"; // Adjust the path as needed
+import logo from "../assets/logo.png"; // Adjust the path as needed
 
 function Navbar() {
   const [isActive, setIsActive] = useState(false);
 
   const menuItems = [
-    { id: 1, link: "/", text: "Home", bgColor: "black" }, // Home background color
-    { id: 2, link: "/about", text: "About Us", bgColor: "#FF5733" }, // 3rd secondary color
-    { id: 3, link: "/skills", text: "Talent", bgColor: "#3d405b" }, // Dark Slate Blue
-    { id: 4, link: "/projects", text: "Our Services", bgColor: "#FFD700" }, // Dark Orange
-    { id: 5, link: "/contact", text: "Contact us", bgColor: "#32CD32" }, // Lime Green
-    { id: 6, link: "/booking", text: "Book a Session", bgColor: "#1E90FF" }, // Dodger Blue
-    { id: 7, link: "/blog", text: "Blog", bgColor: "#800080" }, //PURPLE
+    { id: 1, link: "/", text: "Home", bgColor: "black" },
+    { id: 2, link: "/about", text: "About Us", bgColor: "#FF5733" },
+    { id: 3, link: "/skills", text: "Talent", bgColor: "#3d405b" },
+    { id: 4, link: "/projects", text: "Our Services", bgColor: "#FF8C00" },
+    { id: 5, link: "/contact", text: "Contact us", bgColor: "#32CD32" },
+    { id: 6, link: "/booking", text: "Book a Session", bgColor: "#1E90FF" },
+    { id: 7, link: "/blog", text: "Blog", bgColor: "#800080" },
   ];
 
   const toggleNav = () => {
@@ -30,7 +30,9 @@ function Navbar() {
           width="90"
           height="90"
           className="d-inline-block align-top"
+          style={{ mixBlendMode: "multiply", marginTop: "20px" }}
         />
+
         <div className="brand-text">TRG-ENT</div>
       </div>
       {/* Overlay Menu */}
