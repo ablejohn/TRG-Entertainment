d0f18bfa7e1d2850ac74276c984d5d9a21fec8d5
ABLE JOHN <johnabe410@gmail.com>
2025-04-02 01:22:35 +0100

    latest update

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics"; // Analytics (optional)
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDB020EneBDBpkidg0qdr1eAUfnG69lQJo",
-  authDomain: "trg-ent.firebaseapp.com",
-  projectId: "trg-ent",
-  storageBucket: "trg-ent.firebasestorage.app", // Should be "trg-ent.appspot.com" (see note below)
-  messagingSenderId: "241513022565",
-  appId: "1:241513022565:web:2d58e72ca429643e98728c",
-  measurementId: "G-8DCT8WZHZ3",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
